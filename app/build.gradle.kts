eb4d7be289f50694fcf254b5c0bbb02acc2b10fb
vprasadkot <venkataprasad.kota@gmail.com>
2025-07-27 16:54:27 +0530

    Removing not used code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.poc.pdfreader"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.poc.pdfreader"
-        minSdk = 36
-        targetSdk = 36
+        minSdk = 35
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -51,7 +51,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation("androidx.pdf:pdf-viewer-fragment:1.0.0-alpha10")
+    //implementation("androidx.pdf:pdf-viewer-fragment:1.0.0-alpha10")
     // Apache PDFBox for text extraction
     implementation("org.apache.pdfbox:pdfbox:2.0.29")
     implementation("org.apache.pdfbox:fontbox:2.0.29/fontbox-2.0.29")
